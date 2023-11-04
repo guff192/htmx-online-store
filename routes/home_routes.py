f25b2726559e170b8d70ee14d58001bef1094082
guff192 <kochkons1@ya.ru>
2023-11-04 06:27:43 +0400

    Removed products from home page

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -7,7 +7,7 @@ from fastapi.templating import Jinja2Templates
 
 from routes.auth_routes import google_oauth_user_dependency
 from schema.user_schema import UserBase
-from viewmodels.product_viewmodel import ProductViewModel, product_viewmodel_dependency
+from viewmodels import DefaultViewModel, default_viewmodel_dependency
 
 
 router = APIRouter(prefix='', tags=['Home'])
@@ -22,14 +22,13 @@ def root():
 @router.get('/home', response_class=HTMLResponse, name='home', dependencies=[])
 def home(
     request: Request,
-    product_vm: ProductViewModel= Depends(product_viewmodel_dependency),
     user: UserBase = Depends(google_oauth_user_dependency),
+    default_vm: DefaultViewModel = Depends(default_viewmodel_dependency),
 ):
-    products = product_vm.get_all()
     context_data: dict[str, Any] = {
         'request': request,
         'user': user,
-        **products.build_context()
+        **default_vm.build_context(),
     }
     return templates.TemplateResponse('home.html', context=context_data)
 

8ee52f26201d0ec017da820abcfe2ece924034f3
guff192 <kochkons1@ya.ru>
2023-12-31 06:59:36 +0400

    Added CartViewModel

---
+++ viewmodels/cart_viewmodel.py
@@ -0,0 +1,21 @@
+from typing import Generator
+
+from fastapi import Depends
+from schema.cart_schema import Cart
+from services.cart_service import CartService, cart_service_dependency
+
+
+class CartViewModel:
+    def __init__(self, cart_service: CartService) -> None:
+        self._service = cart_service
+
+    def get_cart(self, user_id: str) -> Cart:
+        return self._service.get_cart(user_id)
+
+
+def cart_viewmodel_dependency(
+    cart_service: CartService = Depends(cart_service_dependency),
+) -> Generator[CartViewModel, None, None]:
+
+    vm = CartViewModel(cart_service)
+    yield vm

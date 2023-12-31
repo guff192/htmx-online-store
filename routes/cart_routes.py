7e19de24ce07466ccfc88bfa99d956ce1af0d7fe
guff192 <kochkons1@ya.ru>
2023-12-31 07:01:05 +0400

    Changed get_cart route, now it works))

--- routes/cart_routes.py
+++ routes/cart_routes.py
@@ -1,26 +1,58 @@
-from fastapi import APIRouter, Depends, Request, Response
-from fastapi.responses import RedirectResponse
+from loguru import logger
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from exceptions.auth_exceptions import ErrUnauthorized
+from routes.auth_routes import google_oauth_user_dependency
+from schema.user_schema import LoggedUser, UserBase
 
-from schema.user_schema import UserBase
 from viewmodels import DefaultViewModel, default_viewmodel_dependency
+from viewmodels.cart_viewmodel import CartViewModel, cart_viewmodel_dependency
 
 
 router = APIRouter(prefix='/cart', tags=['Cart'])
 templates = Jinja2Templates(directory='templates')
 
 
-@router.get('', dependencies=[])
+@router.get('', response_class=HTMLResponse, name='cart')
 def get_cart(
     request: Request,
-    vm: DefaultViewModel = Depends(default_viewmodel_dependency),
+    user: LoggedUser | None = Depends(google_oauth_user_dependency),
+    vm: CartViewModel = Depends(cart_viewmodel_dependency),
 ):
+    if not user:
+        raise ErrUnauthorized()
+
+    cart = vm.get_cart(str(user.id))
+    context = {'request': request, 'user': user, **cart.build_context()}
+
     if request.headers.get('hx-request'):
         return templates.TemplateResponse(
-            'partials/cart.html',
-            context={'request': request, 'user': user})
+            'partials/cart.html', context=context
+        )
+
     return templates.TemplateResponse(
-        'cart.html',
-        context={'request': request, 'user': user, **vm.build_context()}
+        'cart.html', context=context
+    )
+
+
+@router.post('/{product_id}')
+def add_to_cart(
+    request: Request,
+    vm: DefaultViewModel = Depends(default_viewmodel_dependency),
+    user: UserBase | None = Depends(google_oauth_user_dependency),
+):
+    if not user:
+        raise ErrUnauthorized()
+
+    context = vm.build_context()
+    context.update({'user': user})
+    if request.headers.get('hx-request'):
+        return templates.TemplateResponse(
+            'partials/cart.html', context={'request': request, **context}
+        )
+
+    return templates.TemplateResponse(
+        'cart.html', context={'request': request, **context}
     )
 

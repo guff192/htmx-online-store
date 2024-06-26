f7248a1df55637a8b65d41dcc71becea0fa8e7a0
guff192 <kochkons1@ya.ru>
2024-06-26 22:25:52 +0400

    fix: Fixed cart count script

--- static/js/config_to_cart.js
+++ static/js/config_to_cart.js
@@ -1,18 +1,20 @@
 document.addEventListener('htmx:configRequest', function (event) {
     let request_path = event.detail.path;
-    const request_target = event.detail.headers['HX-Target'];
+    const request_target_str = event.detail.headers['HX-Target'].toString();
     const current_url = event.detail.headers['HX-Current-URL'];
+
+    console.log(request_target_str);
 
     if (request_path.includes('/cart/add') || request_path.includes('/cart/remove')) {
         let product_id;
         let configuration_id;
         if (!current_url.includes('/cart')) {
-            product_id = parseInt(request_target.match(/product(\d+)counter/)[1]);
+            product_id = parseInt(request_target_str.match(/product(\d+)counter/)[1]);
             const selected_button = document.querySelector('#product' + product_id + 'priceTabs').querySelector('[aria-selected=true]');
-            configuration_id = parseInt(selected_button.getAttribute('hx-get').split('/').slice(-1)[0]);
+            configuration_id = parseInt(selected_button.getAttribute('hx-get').match(/prices\/(\d+)/)[1]);
         } else {
-            product_id = parseInt(request_target.match(/product(\d+)counter/)[1]);
-            configuration_id = parseInt(request_target.match(/product\d+counter(\d+)/)[1]);
+            product_id = parseInt(request_target_str.match(/product(\d+)counter/)[1]);
+            configuration_id = parseInt(request_target_str.match(/product\d+counter(\d+)/)[1]);
         }
 
         /*

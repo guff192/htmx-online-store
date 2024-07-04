b6395bdc18e3822a2866d99b126b15066d3d3f58
guff192 <kochkons1@ya.ru>
2024-07-04 22:14:33 +0400

    Added functions to work with cookie data

---
+++ routes/cookies.py
@@ -0,0 +1,79 @@
+import urllib.parse
+
+from schema.cart_schema import CartInCookie, CookieCartProduct
+from schema.order_schema import OrderInCookie
+
+
+def get_cart_from_cookies(cookies: dict[str, str]) -> CartInCookie:
+    cookie_cart_str = cookies.get('_cart')
+    if not cookie_cart_str:
+        cookie_cart = CartInCookie(product_list=[])
+    else:
+        cookie_cart = CartInCookie.model_validate_json(cookie_cart_str)
+
+    return cookie_cart
+
+
+def add_product_to_cookie_cart(
+    cookie_cart: CartInCookie, product_id: int, configuration_id: int
+) -> CartInCookie:
+    # adding product to cart
+    products = cookie_cart.product_list
+    try:
+        product = list(filter(
+            lambda p: p.product_id == product_id and p.configuration_id == configuration_id,
+            products
+        ))[0]
+        product.count += 1
+    except IndexError:
+        product = CookieCartProduct(
+            product_id=product_id, configuration_id=configuration_id,
+            count=1,
+        )
+
+    new_products = list(
+        filter(lambda p: p.product_id != product_id, products)
+    )
+    new_products.append(product)
+
+    cookie_cart.product_list = new_products
+
+    return cookie_cart
+
+
+def remove_product_from_cookie_cart(
+    cookie_cart: CartInCookie, product_id: int, configuration_id: int
+) -> CartInCookie:
+    # adding product to cart
+    products = cookie_cart.product_list
+    try:
+        product = list(filter(lambda p: p.product_id == product_id and p.configuration_id == configuration_id,
+                              products))[0]
+        product.count -= 1
+    except IndexError:
+        cookie_cart.product_list.append(
+            CookieCartProduct(
+                product_id=product_id, configuration_id=configuration_id, 
+                count=0
+            )
+        )
+        return cookie_cart
+
+    new_products = list(
+        filter(lambda p: p.product_id != product_id, products)
+    )
+    new_products.append(product) 
+
+    cookie_cart.product_list = new_products
+
+    return cookie_cart
+
+
+def get_order_from_cookies(cookies: dict[str, str]) -> OrderInCookie | None:
+    cookie_order_str = cookies.get('_order')
+    if not cookie_order_str:
+        return None
+    else:
+        cookie_order_str = urllib.parse.unquote(cookie_order_str)
+        return OrderInCookie.model_validate_json(cookie_order_str)
+

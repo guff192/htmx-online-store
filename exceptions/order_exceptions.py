271821d4ad044e32c0aa37bf5644d1f7a9e38c0c
guff192 <kochkons1@ya.ru>
2024-07-04 22:24:02 +0400

    feature: Addeed new order exception (invalid order)

--- exceptions/order_exceptions.py
+++ exceptions/order_exceptions.py
@@ -16,3 +16,11 @@ class ErrUserOrdersNotFound(HTTPException):
             detail='User orders not found'
         )
 
+
+class ErrOrderInvalid(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid order'
+        )
+

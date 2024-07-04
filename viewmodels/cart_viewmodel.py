ce2ee46c14ccec87072cbfc9b50ab5a6b6f21b32
guff192 <kochkons1@ya.ru>
2024-07-04 22:20:09 +0400

    feature: Added method for getting cart from cookies

--- viewmodels/cart_viewmodel.py
+++ viewmodels/cart_viewmodel.py
@@ -1,7 +1,7 @@
 from typing import Generator
 
 from fastapi import Depends
-from schema.cart_schema import Cart
+from schema.cart_schema import Cart, CartInCookie
 from schema.product_schema import ProductInCart
 from services.cart_service import CartService, cart_service_dependency
 
@@ -9,6 +9,9 @@ from services.cart_service import CartService, cart_service_dependency
 class CartViewModel:
     def __init__(self, cart_service: CartService) -> None:
         self._service = cart_service
+
+    def from_cookie(self, cookie_cart: CartInCookie) -> list[ProductInCart]:
+        return self._service.from_cookie_schema(cookie_cart)
 
     def get_cart(self, user_id: str) -> Cart:
         return self._service.get_cart(user_id)

e014dae3e88f4bb6f2d540306f16f14e136d6567
guff192 <kochkons1@ya.ru>
2024-07-04 22:21:53 +0400

    feature: Added new type of exception (access denied)

--- exceptions/auth_exceptions.py
+++ exceptions/auth_exceptions.py
@@ -1,4 +1,32 @@
+from typing import Annotated
+from typing_extensions import Doc
 from fastapi import HTTPException, status
+
+
+class ErrUserNotFound(HTTPException):
+    def __init__(self):
+        super().__init__(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='User not found'
+                )
+
+
+class ErrAccessDenied(HTTPException):
+    def __init__(
+        self,
+        object_name: Annotated[
+            str,
+            Doc(
+                '''Additional human-readable object name to identify object on client-side'''
+            )
+        ] = ''
+    ) -> None:
+        additional_detail = f' to {object_name}' if object_name else ''
+
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='Access denied' + additional_detail
+        )
 
 
 class ErrWrongCredentials(HTTPException):
@@ -9,18 +37,18 @@ class ErrWrongCredentials(HTTPException):
         )
 
 
-class ErrUserNotFound(HTTPException):
-    def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='User not found'
-        )
-
-
 class ErrUnauthorized(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Unauthorized'
+        )
+
+
+class ErrUserInvalid(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid user'
         )
 

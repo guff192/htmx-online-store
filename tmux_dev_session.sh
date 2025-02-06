149b4b61c92f97870e00d0e1761be7d4844d268d
guff192 <kochkons1@ya.ru>
2025-02-07 02:24:24 +0400

    tmux: Made tmux scripts work with any session name

--- tmux_dev_session.sh
+++ tmux_dev_session.sh
@@ -1,36 +1,38 @@
+current_session=$(tmux display-message -p '#S')
+
 # Create tmux windows & set the names
-tmux rename-window -t htmx-online-store:0 'local-server'
-tmux new-window -n 'editor' -t htmx-online-store:
+tmux rename-window -t $current_session:0 'local-server'
+tmux new-window -n 'editor' -t $current_session:
 
 # Split the server window vertically
-tmux split-window -v -t htmx-online-store:local-server.0
+tmux split-window -v -t $current_session:local-server.0
 
 # Split the bottom pane in server window horizontally
-tmux split-window -h -t htmx-online-store:local-server.1
+tmux split-window -h -t $current_session:local-server.1
 
 # Change env variables with sed
 sed -I .bak 's/\(TESTING=\).*/\1False/g' ./.env && rm ./.env.bak
 sed -I .bak 's/\(DB_URL=".*\/\).*"/\1products"/g' ./.env && rm ./.env.bak
 
 # Activate the virtual environment in top pane & clear it
-tmux send-keys -t htmx-online-store:local-server.0 'source ./venv/bin/activate' Enter
-tmux send-keys -t htmx-online-store:local-server.0 C-l
+tmux send-keys -t $current_session:local-server.0 'source ./venv/bin/activate' Enter
+tmux send-keys -t $current_session:local-server.0 C-l
 
 # Run the app in the top pane
-tmux send-keys -t htmx-online-store:local-server.0 'python -B -m app' Enter
+tmux send-keys -t $current_session:local-server.0 'python -B -m app' Enter
 
 # Run public dev endpoint for the app
-tmux send-keys -t htmx-online-store:local-server.1 'ngrok http 42069' Enter
+tmux send-keys -t $current_session:local-server.1 'ngrok http 42069' Enter
 
 # Run the postgres server & connect to it in the right bottom pane & clear it
-tmux send-keys -t htmx-online-store:local-server.2 'pg_ctl -D /usr/local/var/postgresql@17 restart && psql -h localhost -p 5432 -d postgres' Enter
-tmux send-keys -t htmx-online-store:local-server.2 C-l
+tmux send-keys -t $current_session:local-server.2 'pg_ctl -D /usr/local/var/postgresql@17 restart && psql -h localhost -p 5432 -d postgres' Enter
+tmux send-keys -t $current_session:local-server.2 C-l
 
 # Create the 'products' database in the right bottom pane & connect to it & clear it
-tmux send-keys -t htmx-online-store:local-server.2 'CREATE DATABASE products\;' Enter
-tmux send-keys -t htmx-online-store:local-server.2 '\c products' Enter
-tmux send-keys -t htmx-online-store:local-server.2 C-l
+tmux send-keys -t $current_session:local-server.2 'CREATE DATABASE products\;' Enter
+tmux send-keys -t $current_session:local-server.2 '\c products' Enter
+tmux send-keys -t $current_session:local-server.2 C-l
 
 # Activate the virtual environment & open neovim in editor window
-tmux send-keys -t htmx-online-store:editor 'source ./venv/bin/activate' Enter
-tmux send-keys -t htmx-online-store:editor 'nvim' Enter
+tmux send-keys -t $current_session:editor 'source ./venv/bin/activate' Enter
+tmux send-keys -t $current_session:editor 'nvim' Enter
--- tmux_test_session.sh
+++ tmux_test_session.sh
@@ -1,29 +1,31 @@
+current_session=$(tmux display-message -p '#S')
+
 # Create tmux windows & set the names
-tmux rename-window -t htmx-online-store:0 'local-server'
-tmux new-window -n 'editor' -t htmx-online-store:
-tmux new-window -n 'tests' -t htmx-online-store:
+tmux rename-window -t $current_session:0 'local-server'
+tmux new-window -n 'editor' -t $current_session:
+tmux new-window -n 'tests' -t $current_session:
 
 # Change env variables with sed
 sed -I .bak 's/\(TESTING=\).*/\1True/g' ./.env && rm ./.env.bak
 sed -I .bak 's/\(DB_URL=".*\/\).*"/\1test_products"/g' ./.env && rm ./.env.bak
 
 # Split the server window vertically
-tmux split-window -v -t htmx-online-store:local-server.0
+tmux split-window -v -t $current_session:local-server.0
 
 # Run the postgres server & connect to it in the right bottom pane & clear it
-tmux send-keys -t htmx-online-store:local-server.1 'pg_ctl -D /usr/local/var/postgresql@17 restart && psql -h localhost -p 5432 -d postgres' Enter
-tmux send-keys -t htmx-online-store:local-server.1 C-l
+tmux send-keys -t $current_session:local-server.1 'pg_ctl -D /usr/local/var/postgresql@17 restart && psql -h localhost -p 5432 -d postgres' Enter
+tmux send-keys -t $current_session:local-server.1 C-l
 
 # Create the 'products' database in the right bottom pane & connect to it & clear it
-tmux send-keys -t htmx-online-store:local-server.1 'CREATE DATABASE test_products\;' Enter
-tmux send-keys -t htmx-online-store:local-server.1 '\c test_products' Enter
-tmux send-keys -t htmx-online-store:local-server.1 C-l
+tmux send-keys -t $current_session:local-server.1 'CREATE DATABASE test_products\;' Enter
+tmux send-keys -t $current_session:local-server.1 '\c test_products' Enter
+tmux send-keys -t $current_session:local-server.1 C-l
 
 
 # Activate the virtual environment & open neovim in editor window
-tmux send-keys -t htmx-online-store:editor 'source ./venv/bin/activate' Enter
-tmux send-keys -t htmx-online-store:editor 'nvim' Enter
+tmux send-keys -t $current_session:editor 'source ./venv/bin/activate' Enter
+tmux send-keys -t $current_session:editor 'nvim' Enter
 
 # Activate the virtual environment & input the command to run the tests
-tmux send-keys -t htmx-online-store:tests.0 'source ./venv/bin/activate' Enter
-tmux send-keys -t htmx-online-store:tests.0 'pytest tests/'
+tmux send-keys -t $current_session:tests.0 'source ./venv/bin/activate' Enter
+tmux send-keys -t $current_session:tests.0 'pytest tests/'

d7cf91239eaba3f084e752429b41d67b902110d9
guff192 <kochkons1@ya.ru>
2025-05-18 18:16:16 +0400

    fix: Removed redundant function

--- tests/helpers/configurations_helpers.py
+++ tests/helpers/configurations_helpers.py
@@ -18,11 +18,3 @@ def create_available_configs_for_product(db_session: Session, product: ProductDb
 
     return available_configs
 
-
-def get_product_configs_for_ram(db_session: Session, soldered_ram: int, can_add_ram: bool) -> list[ProductConfigurationDbModel]:
-    return db_session.query(ProductConfigurationDbModel).where(and_(
-        ProductConfigurationDbModel.additional_ram == can_add_ram,
-        ProductConfigurationDbModel.soldered_ram == soldered_ram
-    )).all()
-
-

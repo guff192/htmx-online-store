dacbde32246c196a1c7b7d7ada739ce315d8e5eb
guff192 <kochkons1@ya.ru>
2025-05-25 22:00:20 +0400

    feature: Configured domain models for validation from ORM models

--- models/product_configuration.py
+++ models/product_configuration.py
@@ -1,18 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict, Field
 from pydantic_core import Url
 
 
 class ConfigurationType(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int | None
     name: str
     image_url: Url | None = None
 
 
 class ProductConfiguration(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     id: int | None = None
 
     additional_price: int | None = None
     short_name: str | None = None
 
-    type: ConfigurationType
+    type: ConfigurationType = Field(validation_alias='configuration_type')
     value: int | float | str | bool

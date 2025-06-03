497fe72fe336701f40e31cfbfdb692d9895491f4
guff192 <kochkons1@ya.ru>
2025-06-03 18:41:06 +0400

    feature: Rewrote banner repository according to new data model

--- repository/banner_repository.py
+++ repository/banner_repository.py
@@ -1,16 +1,31 @@
 from typing import Generator
 from fastapi import Depends
+from pydantic import ValidationError
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 from db.session import db_dependency, get_db
 from db_models.banner import BannerDbModel
+from models.banner import Banner
 
 
 class BannerRepository:
     def __init__(self, db: Session):
         self.db = db
 
-    def get_all(self) -> list[BannerDbModel]:
-        return self.db.query(BannerDbModel).all()
+    def get_all(self) -> list[Banner]:
+        query = select(BannerDbModel)
+
+        result = self.db.execute(query)
+        orm_banners = result.scalars().all()
+
+        domain_model_banners: list[Banner] = []
+        for orm_banner in orm_banners:
+            try:
+                domain_model_banners.append(Banner.model_validate(orm_banner))
+            except ValidationError:
+                continue
+
+        return domain_model_banners
 
 
 def banner_repository_dependency(

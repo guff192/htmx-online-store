afa1897bf463370d85188da0637a69d2452b3f51
guff192 <kochkons1@ya.ru>
2025-06-04 18:34:30 +0400

    fix: Added default values to domain model fields

--- models/user.py
+++ models/user.py
@@ -10,13 +10,13 @@ class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: UserId
-    google_id: str | None
-    yandex_id: int | None
+    google_id: str | None = None
+    yandex_id: int | None = None
 
     name: str
-    email: str | None
-    phone: str | None
+    email: str | None = None
+    phone: str | None = None
 
-    profile_img_url: Url | None
+    profile_img_url: Url | None = None
 
     is_admin: bool

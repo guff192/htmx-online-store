23c3842e30ff75b4a479016e926453b5317053e5
guff192 <kochkons1@ya.ru>
2025-06-08 21:55:46 +0400

    feature: Added validation setup to cart product domain model

--- models/cart.py
+++ models/cart.py
@@ -1,18 +1,26 @@
 from uuid import UUID
-from pydantic import BaseModel, NonNegativeInt
+from pydantic import AliasPath, BaseModel, ConfigDict, Field, NonNegativeInt
 
 from models.product_configuration import ProductConfiguration
 
 
 class CartProduct(BaseModel):
-    id: int
-    product_id: int
-    quantity: NonNegativeInt
+    model_config = ConfigDict(from_attributes=True)
 
-    name: str
-    price: int
-    selected_configurations: list[ProductConfiguration]
-    
+    id: int | None = None
+    product_id: int
+    quantity: NonNegativeInt = Field(validation_alias="count")
+
+    name: str | None = Field(
+        validation_alias=AliasPath("product", "name"), default=None
+    )
+    price: int | None = Field(
+        validation_alias=AliasPath("product", "price"), default=None
+    )
+    selected_configurations: list[ProductConfiguration] = Field(
+        validation_alias="configurations"
+    )
+
 
 class Cart(BaseModel):
     user_id: UUID
